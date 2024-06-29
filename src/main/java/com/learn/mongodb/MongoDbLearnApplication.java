4feee114c2c2930f12a4961c0d4d629e365f4623
ajaykumar9493 <ajaykumarec.1623@gmail.com>
2024-06-29 19:19:07 +0530

    initial commit

---
+++ src/main/java/com/learn/mongodb/MongoDbLearnApplication.java
@@ -0,0 +1,13 @@
+package com.learn.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbLearnApplication.class, args);
+	}
+
+}
